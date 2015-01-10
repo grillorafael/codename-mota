3baa95b5306e409f96275182cd365549650d87c7
Rafael Grillo <grillorafael@gmail.com>
2015-01-10 10:39:05 -0200

    first commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Codename Mota
+
+## Description
+
+None of your business
+
+## Running the project
+```php -S localhost:8000``` inside the ```www``` folder
---
+++ www/js/game.js
@@ -0,0 +1,21 @@
+(function() {
+    "use strict";
+
+    var game = new Phaser.Game(900, 600, Phaser.AUTO, '', {
+        preload: preload,
+        create: create,
+        update: update
+    });
+
+    function preload() {
+        // TODO
+    }
+
+    function create() {
+        // TODO
+    }
+
+    function update() {
+        // TODO
+    }
+})();
